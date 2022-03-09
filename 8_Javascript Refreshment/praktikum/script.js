9402431d626fcf60b22e507e5d07a6c386344e4f
Syafik Adin <syafik.adin222@gmail.com>
2022-03-09 17:44:12 +0700

    tugas praktikum

---
+++ 8_Javascript Refreshment/praktikum/script.js
@@ -0,0 +1,62 @@
+// var a = 5;
+// let b = "kampus merdeka";
+// const nama = "Budi";
+// let terdaftar = true;
+// let lengkap_arr = [a, b, nama, terdaftar];
+
+// function perkenalan(){
+//     let asal = "indonesia";
+//     return console.log(
+//         "perkenalkan nama saya "+
+//         nama +
+//         " nomor urut " +
+//         a +
+//         " sekarang sedang mengikuti " +
+//         b +
+//         "berasal dari " +
+//         asal
+//     );
+// }
+
+// if(terdaftar === true){
+//     console.log(nama + " terdaftar sebagai kegiatan kampus merdeka")
+// }
+
+// console.log("array = " +nama);
+
+// a = b;
+// // nama = b;
+
+// // console.log("asal diakses = " + asal);
+// console.log("a adalah = " + a);
+// console.log("b adalah = " + b);
+// console.log(perkenalan());
+
+// // Soal 1A
+// // console.log(lengkap_arr[1]); 
+
+// Soal No 3
+// const foo = ['Budi', 'Sita', 'Ayu'];
+// const [a, b, c] = foo;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Soal No 4
+// let bdays = ['10-17', '05-19', '20-19'];
+// let map1 = bdays.map(x => x.replace('-', '/'));
+
+// console.log(map1);
+
+// Soal No 5
+// let value = [1,2,3,4,5,6];
+// let mapKali = value.map(x => x*2);
+
+// console.log(mapKali);
+
+// Soal No 6
+let arr = [1.5, 2.56, 5.1, 12.33];
+let round = arr.map(x => Math.round(x));
+
+console.log(round);
